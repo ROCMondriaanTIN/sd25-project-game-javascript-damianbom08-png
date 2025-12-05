31b417c155b42627c068e1f2cc5fc9b684dc6b43
damianbom08-png <damianbom08@gmail.com>
2025-12-05 14:26:17 +0100

    goed gedeelte gemaakt

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,2 +1,49 @@
+const div0 = document.querySelector("#div0")
+const div1 = document.querySelector("#div1")
+const div2 = document.querySelector("#div2")
+const div3 = document.querySelector("#div3")
+const div4 = document.querySelector("#div4")
+const div5 = document.querySelector("#div5")
+const div6 = document.querySelector("#div6")
+const div7 = document.querySelector("#div7")
+const div8 = document.querySelector("#div8")
+
+div0.addEventListener("click", clickDiv0)
+div1.addEventListener("click", clickDiv1)
+div2.addEventListener("click", clickDiv2)
+div3.addEventListener("click", clickDiv3)
+div4.addEventListener("click", clickDiv4)
+div5.addEventListener("click", clickDiv5)
+div6.addEventListener("click", clickDiv6)
+div7.addEventListener("click", clickDiv7)
+div8.addEventListener("click", clickDiv8)
+function clickDiv0(){
+    console.log("dit is div0")
+}
+function clickDiv1(){
+    console.log("dit is div1")
+}
+function clickDiv2(){
+    console.log("dit is div2")
+}
+function clickDiv3(){
+    console.log("dit is div3")
+}
+function clickDiv4(){
+    console.log("dit is div4")
+}
+function clickDiv5(){
+    console.log("dit is div5")
+}
+function clickDiv6(){
+    console.log("dit is div6")
+}
+function clickDiv7(){
+    console.log("dit is div7")
+}
+function clickDiv8(){
+    console.log("dit is div8")
+}
+
 
 
